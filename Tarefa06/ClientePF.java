1e657a1a71b17df11795cf49dba073d84773a3de
JulianoKenzo <141460497+Capicheee@users.noreply.github.com>
2025-10-16 08:37:53 -0300

    Update ClientePF.java

--- Tarefa06/ClientePF.java
+++ Tarefa06/ClientePF.java
@@ -4,12 +4,10 @@ import java.time.LocalDate;
 
 public class ClientePF extends Cliente {
     private String cpf;
-    private LocalDate dataNascimento;
 
-    public ClientePF(String nome, String email, String cpf, LocalDate dataNascimento) {
+    public ClientePF(String nome, String email, String cpf) {
         super(nome, email);
         this.cpf = cpf;
-        this.dataNascimento = dataNascimento;
     }
 
     public String getCPF() { return cpf; }
